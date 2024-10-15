9f7c6e95b2707e755c540bed01b3751306b5d27c
Luis <luisbarraconsiglieri@gmail.com>
2024-10-15 18:14:01 -0500

    se agrega return emoji

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,12 @@
-declare module "isnt-number" {
+declare module "is-not-number" {
   function isNotNumber(value: any): boolean;
   function isEmojiNotANumber(value: any): boolean;
+  function isNotNumberEmoji(value: any): string;
 
   const _default: {
     isNotNumber: typeof isNotNumber;
     isEmojiNotANumber: typeof isEmojiNotANumber;
+    isNotNumberEmoji: typeof isNotNumberEmoji;
   };
   export = _default;
 }
