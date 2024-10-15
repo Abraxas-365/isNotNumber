3e8aa862e015fbdb210e61af14aa1b1b11926d07
Abraxas-365 <luisfmiranda8@gmail.com>
2024-10-15 18:24:48 -0500

    fix

--- index.d.ts
+++ index.d.ts
@@ -4,19 +4,27 @@ declare module "isnt-number" {
    * @param value The value to check.
    * @returns True if the value is not a number, false otherwise.
    */
-  export function isNotNumber(value: any): boolean;
+  function isNotNumber(value: any): boolean;
 
   /**
    * Checks if the given value is not a number emoji.
    * @param value The value to check.
    * @returns True if the value is not a number emoji, false otherwise.
    */
-  export function isEmojiNotANumber(value: any): boolean;
+  function isEmojiNotANumber(value: any): boolean;
 
   /**
    * Returns an emoji based on whether the input is not a number.
    * @param value The value to check.
    * @returns A smiling face emoji if the value is a number, a frowning face emoji otherwise.
    */
-  export function isNotNumberEmoji(value: any): string;
+  function isNotNumberEmoji(value: any): string;
+
+  const _default: {
+    isNotNumber: typeof isNotNumber;
+    isEmojiNotANumber: typeof isEmojiNotANumber;
+    isNotNumberEmoji: typeof isNotNumberEmoji;
+  };
+
+  export = _default;
 }
