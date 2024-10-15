6ee0ba5532de0d69802318fc5e9aaac78e415fc1
Abraxas-365 <luisfmiranda8@gmail.com>
2024-10-15 18:16:48 -0500

    fix

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,5 @@
-declare module "is-not-number" {
-  function isNotNumber(value: any): boolean;
-  function isEmojiNotANumber(value: any): boolean;
-  function isNotNumberEmoji(value: any): string;
-
-  const _default: {
-    isNotNumber: typeof isNotNumber;
-    isEmojiNotANumber: typeof isEmojiNotANumber;
-    isNotNumberEmoji: typeof isNotNumberEmoji;
-  };
-  export = _default;
+declare module "isnt-number" {
+  export function isNotNumber(value: any): boolean;
+  export function isEmojiNotANumber(value: any): boolean;
+  export function isNotNumberEmoji(value: any): string;
 }
